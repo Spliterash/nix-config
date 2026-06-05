af9c0876dec2af4e33b368dfb5093b66453285da
Spliterash <me@spliterash.ru>
2026-06-05 05:39:42 +0300

    Update

--- home/plasma.nix
+++ home/plasma.nix
@@ -6,6 +6,18 @@
 
   programs.plasma = {
     enable = true;
-    configFile."ksmserverrc"."General"."loginMode" = "emptySession";
+    configFile = {
+      "ksmserverrc"."General"."loginMode" = "emptySession";
+      # Alt+Shift в alternative-слот (release-trigger), чтобы не угонять фокус
+      # в меню приложений. См. https://discuss.kde.org/t/22698
+      "kglobalshortcutsrc"."KDE Keyboard Layout Switcher"."Switch to Next Keyboard Layout" =
+        "none,Alt+Shift\tnone\tSwitch to Next Keyboard Layout";
+    };
+  };
+  # Фигня чтобы shiftalt и altshift норм работали, а то заебало
+  gtk = {
+    enable = true;
+    gtk3.extraConfig.gtk-enable-mnemonics = 0;
+    gtk4.extraConfig.gtk-enable-mnemonics = 0;
   };
 }
