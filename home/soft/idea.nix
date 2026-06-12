a78fa678b761556aed3a3e02f37c52bb34c992c6
Spliterash <me@spliterash.ru>
2026-06-12 20:22:37 +0300

    Фикс идеи

--- home/soft/idea.nix
+++ home/soft/idea.nix
@@ -1,18 +1,15 @@
 { inputs, pkgs, ... }:
 let
-  #? IntelliJ IDEA — Community was discontinued by JetBrains, idea-oss is the
-  #? open-source successor (idea-ultimate / idea also available).
-  #? https://blog.jetbrains.com/idea/2025/07/intellij-idea-unified-distribution-plan/
-
-  #? Plugins: nixpkgs no longer resolves jetbrains plugins by id (since 26.11),
-  #? so we pull derivations from nix-jetbrains-plugins. The IDE version is
-  #? auto-detected; find plugin ids at the bottom of a Marketplace page.
-  #? https://github.com/theCapypara/nix-jetbrains-plugins
   withPlugins = inputs.nix-jetbrains-plugins.lib.buildIdeWithPlugins pkgs;
+
+  #? Фикс всплывашек, убрать когда пофиксят
+  idea = pkgs.jetbrains.idea.override {
+    vmopts = "-Dawt.toolkit.name=XToolkit";
+  };
 in
 {
   home.packages = [
-    (withPlugins pkgs.jetbrains.idea [
+    (withPlugins idea [
       # Translation — https://plugins.jetbrains.com/plugin/8579-translation
       "cn.yiiguxing.plugin.translate"
     ])
