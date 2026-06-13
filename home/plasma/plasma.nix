78aec7fd5e0bca659c2e205fc7e1291b5d6e734b
Spliterash <me@spliterash.ru>
2026-06-13 15:46:06 +0300

    Блок звука фуррифокса, чтобы экран гасился

--- home/plasma/plasma.nix
+++ home/plasma/plasma.nix
@@ -197,6 +197,10 @@ in
       "spectaclerc"."GuiConfig"."captureMode" = 0;
       "spectaclerc"."GuiConfig"."quitAfterSaveCopyExport" = true;
       "plasma_calendar_holiday_regions"."General"."selectedRegions" = "ru_ru";
+
+      # Не даём фуррифоксу держать включённый экран, если играет музыка
+      # То что тут текстом задано кринж кнч, но оно реал так в конфиге лежит
+      "powerdevilrc"."Inhibitions"."BlockedInhibitions" = "firefox:Воспроизведение звука";
     };
   };
 }
