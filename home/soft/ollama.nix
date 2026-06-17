bcc842c46a9c918d94daaa7124b29d15b66146f6
Spliterash <me@spliterash.ru>
2026-06-17 21:28:22 +0300

    Ollama резервация

--- home/soft/ollama.nix
+++ home/soft/ollama.nix
@@ -4,6 +4,7 @@
     acceleration = "rocm";
     environmentVariables = {
       OLLAMA_MODELS = "/mnt/data/Software/Ollama/";
+      OLLAMA_GPU_OVERHEAD = toString (512 * 1024 * 1024); # ≈ 0.6 ГБ
     };
   };
 }
