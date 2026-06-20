17b2b37ef582d787c4360b0cbb526ae09f7cf980
Spliterash <me@spliterash.ru>
2026-06-20 20:16:46 +0300

    Более большой буффер обмена

--- home/plasma/plasma.nix
+++ home/plasma/plasma.nix
@@ -207,6 +207,12 @@ in
       # Не даём фуррифоксу держать включённый экран, если играет музыка
       # То что тут текстом задано кринж кнч, но оно реал так в конфиге лежит
       "powerdevilrc"."Inhibitions"."BlockedInhibitions" = "firefox:Воспроизведение звука";
+
+      # Буфер обмена (Klipper). История лежит в ~/.local/share/klipper/history3.sqlite.
+      # KeepClipboardContents форсит сохранение истории между перезапусками (дефолт KDE
+      # тоже true, но фиксируем явно — overrideConfig=false не трогает необъявленные ключи).
+      "klipperrc"."General"."MaxClipItems" = 256; # дефолт 20, максимум 2048
+      "klipperrc"."General"."KeepClipboardContents" = true;
     };
   };
 }
